171467d195cfccb0876a0297ad968f125fea882b
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2025-05-08 15:15:23 +0530

    changes in Home component, if there is no post then show empty

--- src/root/pages/Home.jsx
+++ src/root/pages/Home.jsx
@@ -12,11 +12,17 @@ const Home = () => {
         <div className='max-w-screen-sm flex flex-col items-center w-full gap-6 sm:gap-8'>
           <h2 className='text-xl sm:text-2xl text-left w-full'>Home Feed</h2>
 
-          <div className='flex flex-col gap-4'>
-            {posts.map((post, index)=>(
-              <PostCard key={index} post={post}/>
-            ))}
-          </div>
+          {
+            posts.length==0 ? 
+            <p className='bg-gray-50 px-8 py-4 rounded'>No posts yet. Please create one to get started.</p> 
+            :
+            <div className='flex flex-col gap-4'>
+              {posts.map((post, index)=>(
+                <PostCard key={index} post={post}/>
+              ))}
+            </div>
+          }
+          
         </div>
       </div>      
     </div>
