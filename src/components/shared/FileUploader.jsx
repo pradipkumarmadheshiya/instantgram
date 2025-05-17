62647d5b545a9ec202759ad2a05f57acf64eb1ef
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2025-05-17 17:43:04 +0530

    update in fileUploader

--- src/components/shared/FileUploader.jsx
+++ src/components/shared/FileUploader.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useAppContext } from '../../context/AppContext'
-import { ChevronLeft } from 'lucide-react'
 
 const FileUploader = () => {
 
@@ -18,7 +17,7 @@ const FileUploader = () => {
   return (
     <form className="p-4 flex flex-col justify-center items-center">
       <label className="mb-2 text-sm font-medium text-gray-700" htmlFor="file_input">
-        <span className='flex items-center'>Upload Image (JPG, PNG, SVG) <ChevronLeft className='w-5'/> 1 mb</span>
+        Upload Image (JPG, PNG, SVG)
       </label>
 
       <input 
